219c2146ed16b015462bfdc8c9c3dc90e16e0e53
Philip Rebeschini <philrebeschini@gmail.com>
2018-08-20 17:00:06 +0200

    Make Sudoku size variable.

--- Sudoku.hpp
+++ Sudoku.hpp
@@ -4,13 +4,16 @@
 
 class Sudoku {
 private:
-    int grid[9][9] {};
+    int row_size, col_size, box_size;
+    int **grid;
     bool alreadyInRow(int, int) const;
     bool alreadyInCol(int, int) const;
     bool alreadyInBox(int, int, int) const;
     bool viable(int, int, int) const;
     bool findNextEmptyCell(int&, int&) const;
 public:
+    Sudoku(int, int, int);
+    ~Sudoku();
     bool solve();
     friend std::istream& operator>>(std::istream&, Sudoku&);
     friend std::ostream& operator<<(std::ostream&, const Sudoku&);
